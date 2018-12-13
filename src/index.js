facd4754cdaa3e43f5dca5a356ac600e5357328a
Mister Parker <silentninja.p@yahoo.com>
2018-12-13 14:22:49 +0530

    1) calculate winner function added, this function contatins array of all position cominatinos for winning. 2) alternate turns of 'X' and 'O' added. this was done by adding xIsNext in board constructor, so initilazing as X's turn as xIsNext as true we will make it false and whenever it is false we will check the status while redering and decide that its's O's turn.

--- src/index.js
+++ src/index.js
@@ -38,6 +38,27 @@ function Square(props){
   )
 }
 
+function CalculateWinner(squares){
+  const lines = [   //all the combinations of win
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for (let i = 0; i <lines.length; i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a]===squares[b] &&  //if positions have same character
+      squares[a]===squares[c]){
+        return squares [a];
+      }
+  }
+  return null;
+}
+
     // NOTE : to share data between two chilren, we share the space with 
     // parent instead, and parent share it back to the child using props
     // this keep child components in sync with each other, basically the
@@ -49,6 +70,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,    
     };
   }
 
@@ -60,8 +82,11 @@ class Board extends React.Component {
   handleClick(i){
     const squares = this.state.squares.slice();
     // .slice() to create copy, to modify copy instead of existing array
-    squares[i] = 'X';
-    this.setState({squares : squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares : squares,
+      xIsNext : !this.state.xIsNext,
+    });
   }
   renderSquare(i) {
   //  return <Square value={i} />; this one was for giving 
@@ -74,7 +99,14 @@ class Board extends React.Component {
   }
   
   render() {
-    const status = 'Next player: X';
+    const winner = CalculateWinner(this.state.squares);
+    let status;
+    if(winner){
+      status = 'Winner : '+ winner;
+    }else {
+      status = 'Next player: ' +
+      (this.state.xIsNext ? 'X' : 'O');
+    } 
 
     return (
       <div>
